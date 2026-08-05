09ed87497e9f140e16f539ac06bebbeb17c39e30
Nitin Baranwal <nitinkumarbaranawal@gmail.com>
2026-08-05 08:28:32 +0530

    Auto update 2026-08-05--08:28:32

--- Sol.java
+++ Sol.java
@@ -1,28 +1,26 @@
 import java.io.*;
 import java.util.*;
 public class Sol {
-   
+   static int[] dp;
    public static void main(String[] args) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       PrintWriter out = new PrintWriter(System.out);
-      long q = Long.parseLong(br.readLine());
-      while(q-- > 0) {
-         long a = Long.parseLong(br.readLine());
-         long idx=9;
-         long num = 9;
-         for(int i=1;i<19;i++){
-            if(idx>=a) break;
-            idx += Math.pow(10,i)*9*(i+1); 
-            num += Math.pow(10,i)*9; 
-         }
-         int size=(int)Math.log10(num)+1;
-         long num2 = (num - (idx-a)/size);
-         int digit=(int)((idx-a)%size);
-         System.out.println(num2);
-         out.println(String.valueOf(num2).charAt(size-digit-1));
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n = Integer.parseInt(st.nextToken());
+      int x = Integer.parseInt(st.nextToken());
+      int[] arr = new int[n];
+      st = new StringTokenizer(br.readLine());
+      for(int i = 0; i < n; i++) {
+         arr[i] = Integer.parseInt(st.nextToken());
       }
-      
+      dp = new int[n];
+      Arrays.fill(dp, -1);
+      int ans = solve(arr, x, 0);
+      out.println(ans);
       out.flush();
+   }
+   private int solve(int[] arr,int x,int idx){
+      if
    }
    
 }
