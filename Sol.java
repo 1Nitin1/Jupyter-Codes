aa4c92c156128f664d9c2e4c53b2b9c7fe3c4a9e
1Nitin1 <nitinkumarbaranawal@gmail.com>
2026-09-03 14:18:53 +0000

    Auto sync from VM on 2026-09-03 14:18:53

--- Sol.java
+++ Sol.java
@@ -1,5 +1,5 @@
 public class Sol {
    public static void main(String[] args) {
-      System.out.println("Hello, I am Nitin Baranwal");
+      System.out.println("Hello, I am Nitin Baranwal. I am a Data Scientist and I have a keen interest in Machine Learning, Deep Learning, and Artificial Intelligence. I have experience working with various ML algorithms and frameworks, and I enjoy solving complex problems using data-driven approaches.");
    }
 }
